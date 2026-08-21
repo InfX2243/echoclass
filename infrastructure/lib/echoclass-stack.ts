8f4200d14c6877850d0f229f68b5cca158b3325f
InfX2243 <abid.231446.it@mhssce.ac.in>
2026-08-21 23:46:01 +0530

    feat(db): add development DynamoDB application table

--- infrastructure/lib/echoclass-stack.ts
+++ infrastructure/lib/echoclass-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { aws_dynamodb as dynamodb } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export interface EchoClassStackProps extends cdk.StackProps {
@@ -15,6 +16,38 @@ export class EchoClassStack extends cdk.Stack {
       Environment: environmentName,
       Project: 'EchoClass',
     };
+
+    const applicationTable = new dynamodb.Table(this, 'ApplicationTable', {
+      tableName: `EchoClass-${environmentName}-application`,
+      partitionKey: { name: 'PK', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'SK', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      encryption: dynamodb.TableEncryption.AWS_MANAGED,
+      pointInTimeRecoverySpecification: {
+        pointInTimeRecoveryEnabled: true,
+      },
+      removalPolicy: environmentName === 'prod' ? cdk.RemovalPolicy.RETAIN : cdk.RemovalPolicy.DESTROY,
+    });
+
+    applicationTable.addGlobalSecondaryIndex({
+      indexName: 'GSI1',
+      partitionKey: { name: 'GSI1PK', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'GSI1SK', type: dynamodb.AttributeType.STRING },
+      projectionType: dynamodb.ProjectionType.ALL,
+    });
+
+    applicationTable.addGlobalSecondaryIndex({
+      indexName: 'GSI2',
+      partitionKey: { name: 'GSI2PK', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'GSI2SK', type: dynamodb.AttributeType.STRING },
+      projectionType: dynamodb.ProjectionType.ALL,
+    });
+
+    new cdk.CfnOutput(this, 'ApplicationTableName', {
+      value: applicationTable.tableName,
+      description: 'EchoClass application DynamoDB table name',
+      exportName: `${environmentName}-EchoClass-ApplicationTableName`,
+    });
 
     new cdk.CfnOutput(this, 'EnvironmentName', {
       value: environmentName,
